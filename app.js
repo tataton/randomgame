fa2bacb281d1977f5099be0ccb5ba6daee1f0ed9
Deforis Nash <defhnash@gmail.com>
2016-12-08 14:50:51 -0600

    initial server set up

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-app.listen(1776, function(){
-console.log('Listening!');
-});
+var bodyParser = require('body-parser');
+var urlEncodedParser = bodyParser.urlencoded({extended:false});
+var maxNum;
+
 app.use(express.static('public'));
+
+app.listen(1776, function(){
+  console.log('Listening!');
+});
+
+app.post('/getMax', urlEncodedParser, function(req, res){
+  console.log('/getMax url hit');
+  console.log('req.body:',req.body);
+  res.send(req.body);
+  maxNum = req.body.num;
+  console.log(maxNum);
+});
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,27 @@
 console.log("SOurced");
+
+var postMaxNum = function(num) {
+  $.ajax({
+    type: "POST",
+    data: {num: num},
+    url: '/getMax',
+    success: function(response) {
+      console.log('postMaxNum ajax success');
+    },
+    error: function(){
+      console.log('get max ajax error');
+    }
+  });
+}; // end postMaxNum
+
 $(document).ready(function(){
   console.log('JQ');
-
+  //event listeners
+  $('#startButton').on('click', function(){
+    console.log('start clicked');
+    var maxNum = $('#maxNumIn').val();
+    console.log(maxNum);
+    postMaxNum(maxNum);
+  }); // end #startButtonnp
 
 });
