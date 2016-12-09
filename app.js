2635b008b5c3976866a1c6413fa55db0ef710177
T. Andrew Taton <thomas.andrew.taton@gmail.com>
2016-12-09 10:00:53 -0600

    PlayerArray works.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
-var urlEncodedParser = bodyParser.urlencoded({extended:false});
+var urlEncodedParser = bodyParser.urlencoded({extended:true});
 var maxNum;
 
 
@@ -15,17 +15,17 @@ app.use(express.static('public'));
 
 app.listen(1776, function(){
   console.log('Listening!');
-});
+}); //app listen end
 
-app.post('/getMax', urlEncodedParser, function(req, res){
-  console.log('/getMax url hit');
+app.post('/postMax', urlEncodedParser, function(req, res){
+  console.log('/postMax url hit');
   console.log('req.body:',req.body);
   res.send(req.body);
   maxNum = req.body.num;
   var gameNumber = rand(maxNum);
   console.log(maxNum);
   console.log(gameNumber);
-});
+}); //postMax end
 
 
 
@@ -33,4 +33,4 @@ app.post('/postInputs', urlEncodedParser, function(req, res){
   console.log('/postInputs url hit');
   console.log('req.body:',req.body);
   res.send(req.body);
-});
+}); //postInputs end
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,10 +1,19 @@
-console.log("SOurced");
+console.log("Sourced");
+
+var playersArray = [];
+
+function Player(playerName, guess, howClose){
+  this.playerName = playerName;
+  this.guess = guess;
+  this.howClose = howClose;
+  playersArray.push(this);
+}
 
 var postMaxNum = function(num) {
   $.ajax({
     type: "POST",
     data: {num: num},
-    url: '/getMax',
+    url: '/postMax',
     success: function(response) {
       console.log('postMaxNum ajax success');
     },
@@ -36,16 +45,21 @@ $(document).ready(function(){
     var maxNum = $('#maxNumIn').val();
     console.log(maxNum);
     postMaxNum(maxNum);
+    var playerOne = new Player("Player 1");
+    var playerTwo = new Player("Player 2");
+    var playerThree = new Player("Player 3");
+    var playerFour = new Player("Player 4");
+    console.log("player test on Start:", playerOne, playersArray);
   }); // end #startButtonnp
+
   $('#submit').on('click', function(){
     console.log('submit clicked');
-    var inputs = {
-      one:$('#playerOne').val(),
-      two:$('#playerTwo').val(),
-      three:$('#playerThree').val(),
-      four:$('#playerFour').val()
-    };
-    postInputs(inputs);
+    playersArray[0].guess = $('#playerOne').val();
+    playersArray[1].guess = $('#playerTwo').val();
+    playersArray[2].guess = $('#playerThree').val();
+    playersArray[3].guess = $('#playerFour').val();
+    console.log("player test on Submit:", playerOne, playersArray);
+    postInputs(playersArray);
   }); // end #startButtonnp
 
 });
